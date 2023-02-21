e6403147ac9a0d324640422f52830ec39f86d10c
IanTapia <Adrik.tapiavalero48@gmail.com>
2023-02-21 07:53:05 -0600

    inicia commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function validateForm ( ) 
+
+    var name =document.getElementById("name").value 
+    var password=document.getElementById("password").value 
+     if (name == "Adrik" && password == "0607") {
+        
+     }
